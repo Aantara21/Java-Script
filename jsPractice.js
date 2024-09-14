9bb8d4d72cbfe31cc5de1378bc23cf813f29034e
Aantara21 <antarabaidya21@gmail.com>
2024-09-14 11:17:44 +0600

    condition

---
+++ jsPractice.js
@@ -0,0 +1,23 @@
+const money = 1000;
+const bill = 700;
+const back = money - bill ;
+// console.log (back);
+
+const Mathematics = 75.25;
+const Biology = 65;
+const Chemistry = 80;
+const Physics = 35.45;
+const Bangla = 99.50;
+const sum = Mathematics + Biology + Chemistry + Physics + Bangla;
+const avrg = sum / 5;
+// console.log (avrg.toFixed(2));
+
+const reminder = 119 % 5;
+// console.log('the remainder', reminder);
+let brgr = 450;
+if (brgr > 500) {
+    console.log ('free Coke');
+}
+else{
+    console.log (' Else Coke: 30tk');
+}
